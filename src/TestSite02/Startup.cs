75eb3edc85e99ade52f88d340cc1848c4ef8eb17
oledb <zf0qwe@gmail.com>
2017-04-07 12:20:53 +0300

    Added Mvc to project

--- src/TestSite02/Startup.cs
+++ src/TestSite02/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,7 +28,7 @@ namespace TestSite02
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc();
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -45,11 +47,12 @@ namespace TestSite02
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routers =>
             {
-                var logger = loggerFactory.CreateLogger("Information");
-                logger.LogWarning("Hello world!");
-                await context.Response.WriteAsync(env.ApplicationName);
+                routers.MapRoute(
+                    name: "Default",
+                    template: "{controller=Home}/{Action=Index}/{id?}"
+                );
             });
         }
     }
