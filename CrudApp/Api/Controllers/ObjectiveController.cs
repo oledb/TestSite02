9468f95fffc4414439642280aabc5a10a3353941
oledb <zf0qwe@gmail.com>
2017-04-20 11:32:27 +0300

    Added Api ObjectiveController and tests for it

---
+++ CrudApp/Api/Controllers/ObjectiveController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestSite02.AbstractModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CrudApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ObjectiveController : Controller
+    {
+        private IObjectives _objectives;
+
+        public ObjectiveController(IObjectives obj)
+        {
+            this._objectives = obj;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Objective> Get()
+        {
+            return _objectives.GetObjectives();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/UnitTests/ApiControllerTest/ObjectiveControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Xunit;
+using TestSite02.AbstractModel;
+using TestSite02.FaketModel;
+using CrudApp.Api.Controllers;
+
+namespace UnitTests
+{
+    
+    public class ObjectiveControllerTest
+    {
+        private ObjectiveController Initilize()
+        {
+            IObjectives obj = new FakeObjectives();
+            return new ObjectiveController(obj);
+        }
+
+        [Fact]
+        public void ObjectiveControllerGet()
+        {
+            //Arrange
+            var controller = Initilize();
+
+            //Act
+            var list = (List<Objective>)controller.Get();
+
+            //Assert
+            Assert.Equal(3, list.Count);
+            Assert.Equal("Test 02", list[1].Name);
+        }
+    }
+}
