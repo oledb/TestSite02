8fa36403ec42a636fd430b5ef78b39cf04c71125
oledb <zf0qwe@gmail.com>
2017-04-20 10:31:26 +0300

    Added UnitTest project

---
+++ src/UnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Xunit;
+
+namespace UnitTests
+{
+    
+    public class Class1
+    {
+        [Fact]
+        public void TestSimpe()
+        {
+        }
+    }
+}
