319887a7fdef7c0c28e58a9b18b154b626378cb0
oledb <zf0qwe@gmail.com>
2017-04-25 11:50:45 +0300

    added post method to XhrModel

--- CrudApp/Scripts/core/XhrModel.js
+++ CrudApp/Scripts/core/XhrModel.js
@@ -1,22 +1,41 @@
 var XhrModel = (function () {
-    function XhrModel(Url) {
+    function XhrModel(Url, error) {
         this.Url = Url;
+        this.error = error;
+        this.timeout = 5000;
     }
     XhrModel.prototype.Get = function (success) {
         $.ajax({
+            type: 'get',
             url: this.Url,
-            success: success
+            success: success,
+            error: this.error,
+            timeout: this.timeout
+        });
+    };
+    XhrModel.prototype.Post = function (value, success) {
+        $.ajax({
+            type: 'post',
+            url: this.Url,
+            success: success,
+            error: this.error,
+            data: JSON.stringify(value),
+            dataType: "json",
+            timeout: this.timeout
         });
     };
     return XhrModel;
 }());
 $("window").ready(function () {
-    var t = new XhrModel("/api/Objective");
-    console.log("start xhr");
+    var t = new XhrModel("/api/Objective", function () {
+        console.log("Cannot execute request!!!");
+    });
+    t.Post({ Name: "Test task 007" }, function () {
+        console.log("Post success");
+    });
     t.Get(function (rslt) {
-        console.log("output");
+        console.log("get query");
         console.log(rslt);
     });
-    console.log("end xhr");
 });
 //# sourceMappingURL=XhrModel.js.map
