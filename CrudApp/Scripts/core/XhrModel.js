7701eda6359b5f266128db5b415aa44378b146ff
oledb <zf0qwe@gmail.com>
2017-04-25 20:54:41 +0300

    Revert "added post method to XhrModel"
    This reverts commit 319887a7fdef7c0c28e58a9b18b154b626378cb0.

--- CrudApp/Scripts/core/XhrModel.js
+++ CrudApp/Scripts/core/XhrModel.js
@@ -1,41 +1,22 @@
 var XhrModel = (function () {
-    function XhrModel(Url, error) {
+    function XhrModel(Url) {
         this.Url = Url;
-        this.error = error;
-        this.timeout = 5000;
     }
     XhrModel.prototype.Get = function (success) {
         $.ajax({
-            type: 'get',
             url: this.Url,
-            success: success,
-            error: this.error,
-            timeout: this.timeout
-        });
-    };
-    XhrModel.prototype.Post = function (value, success) {
-        $.ajax({
-            type: 'post',
-            url: this.Url,
-            success: success,
-            error: this.error,
-            data: JSON.stringify(value),
-            dataType: "json",
-            timeout: this.timeout
+            success: success
         });
     };
     return XhrModel;
 }());
 $("window").ready(function () {
-    var t = new XhrModel("/api/Objective", function () {
-        console.log("Cannot execute request!!!");
-    });
-    t.Post({ Name: "Test task 007" }, function () {
-        console.log("Post success");
-    });
+    var t = new XhrModel("/api/Objective");
+    console.log("start xhr");
     t.Get(function (rslt) {
-        console.log("get query");
+        console.log("output");
         console.log(rslt);
     });
+    console.log("end xhr");
 });
 //# sourceMappingURL=XhrModel.js.map
--- CrudApp/Scripts/core/XhrModel.ts
+++ CrudApp/Scripts/core/XhrModel.ts
@@ -1,43 +1,18 @@
 ﻿/// <reference path="../jquerydef/index.d.ts"/>
 
 class XhrModel {
-    constructor(protected Url: string, protected error: () => void) {
+    constructor(protected Url: string) {
     }
-    private readonly timeout = 5000;
     public Get(success: (result) => void) {
         $.ajax({
-            type: 'get',
             url: this.Url,
-            success: success,
-            error: this.error,
-            timeout: this.timeout
-        });
-    }
-
-    public Post(value: any, success: (result) => void) {
-        $.ajax({
-            type: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            url: this.Url,
-            success: success,
-            error: this.error,
-            data: JSON.stringify(value),
-            dataType: "json",
-            timeout: this.timeout
+            success: success
         });
     }
 }
 
 $("window").ready(() => {
-    var t = new XhrModel("/api/Objective", () => {
-        console.log("Cannot execute request!!!");
-    });
-    t.Post({ Name: "Test task 007" }, () => {
-        console.log("Post success");
-    });
+    var t = new XhrModel("/api/Objective");
     t.Get((rslt) => {
         console.log("get query");
         console.log(rslt);
