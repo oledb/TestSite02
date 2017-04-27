79fed5a97f6619047c8b7a84c621c09433a28791
oledb <zf0qwe@gmail.com>
2017-04-27 19:31:27 +0300

    Added listview code

--- CrudApp/Scripts/core/ListView.js
+++ CrudApp/Scripts/core/ListView.js
@@ -1,20 +1,40 @@
+var _this = this;
 var ListView = (function () {
     function ListView(app) {
         this.app = app;
-        this.list = "ListView_crud";
+        this.listClass = "ListView";
+        this.removeButtonClass = "ListView-RemoveBtn";
+        this.completeButtonClass = "ListView-CompleteBtn";
+        this.controlButtonsClass = "ListView-Controls";
         this.CreateList();
-        this.CreateListElements();
+        this.CreateListElements("hello", 0);
+        this.CreateListElements("my world", 1);
     }
     ListView.prototype.CreateList = function () {
-        $(this.app).append('<ul class="' + this.list + '"></ul>');
+        $(this.app).append("<ul class=\"" + this.listClass + " w3-ul w3-card\"></ul>");
     };
-    ListView.prototype.CreateListElements = function () {
-        $("." + this.list).append("<li>Hello</li>");
-        $("." + this.list).append("<li>World</li>");
+    ListView.prototype.CreateListElements = function (text, index, cssClass) {
+        if (cssClass === void 0) { cssClass = "w3-teal"; }
+        $("." + this.listClass).append("<li class=\"" + cssClass + " w3-display-container\">\n            " + text + " " + this.GetContorlButtons() + "\n        </li>");
+    };
+    ListView.prototype.GetContorlButtons = function () {
+        return "<div class=\"" + this.controlButtonsClass + " w3-display-right\">\n                    <span class=\"" + this.completeButtonClass + " w3-button w3-transparent\">Complete</span>\n                    <span class=\"" + this.removeButtonClass + " w3-button w3-transparent\">Remove</span>\n                </div>";
     };
     return ListView;
 }());
-$("documnet").ready(function () {
+$("window").ready(function () {
     var list = new ListView(".test_list");
+    $(".tl-ctr").hide();
+    $(".tl-li").on("mouseout", function (e) {
+        e.stopPropagation();
+        $(".tl-ctr").hide();
+    });
+    $(".tl-li").on("mouseover", function (e) {
+        e.stopPropagation();
+        $(".tl-ctr", _this).show();
+    });
+    $(".tl-rm").on("click", function () {
+        $(_this).parent();
+    });
 });
 //# sourceMappingURL=ListView.js.map
--- CrudApp/Scripts/core/ListView.ts
+++ CrudApp/Scripts/core/ListView.ts
@@ -1,20 +1,45 @@
 ﻿/// <reference path="../jquerydef/index.d.ts"/>
 
 class ListView {
-    private readonly list = "ListView_crud";
+    public readonly listClass = "ListView";
+    public readonly removeButtonClass = "ListView-RemoveBtn";
+    public readonly completeButtonClass = "ListView-CompleteBtn";
+    public readonly controlButtonsClass = "ListView-Controls";
     constructor(protected app: string) {
         this.CreateList();
-        this.CreateListElements();
     }
     private CreateList() {
-        $(this.app).append('<ul class="' + this.list + '"></ul>');
+        $(this.app).append(`<ul class="${this.listClass} w3-ul w3-card"></ul>`);
     }
-    private CreateListElements() {
-        $("." + this.list).append("<li>Hello</li>");
-        $("." + this.list).append("<li>World</li>");
+    public CreateListElements(text: string, index: number, cssClass: string = "w3-teal") {
+        $("." + this.listClass).append(`<li class="${cssClass} w3-display-container">
+            ${text} ${this.GetContorlButtons()}
+        </li>`);
+    }
+
+    private GetContorlButtons(): string{
+        return `<div class="${this.controlButtonsClass} w3-display-right">
+                    <span class="${this.completeButtonClass} w3-button w3-transparent">Complete</span>
+                    <span class="${this.removeButtonClass} w3-button w3-transparent">Remove</span>
+                </div>`;
     }
 }
 
-$("documnet").ready(() => {
+$("window").ready(() => {
     var list = new ListView(".test_list");
+    list.CreateListElements("Hello", 0);
+    list.CreateListElements("My beutiful world");
+    $(".tl-ctr").hide();
+
+    $(".tl-li").on("mouseout", (e) => {
+        e.stopPropagation();
+        $(".tl-ctr").hide();
+    });
+    $(".tl-li").on("mouseover", (e) => {
+        e.stopPropagation();
+        $(".tl-ctr", this).show();
+    });
+    $(".tl-rm").on("click", () => {
+        $(this).parent()
+    });
 });
