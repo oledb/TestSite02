3fc622084c9bf3c7e3b53bccf00c8e67d1d1ff9d
oledb <zf0qwe@gmail.com>
2017-05-09 18:07:40 +0300

    Added login method

--- CrudApp/Pages/Controllers/AccountController.cs
+++ CrudApp/Pages/Controllers/AccountController.cs
@@ -62,6 +62,36 @@ namespace CrudApp.Pages.Controllers
         }
 
         [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager
+                    .PasswordSignInAsync(
+                    model.Email,
+                    model.Password,
+                    model.RememberMe,
+                    lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                   // _logger.LogInformation(1, "User logged in.");
+                    return RedirectToLocal("~/Pages/Default/Objective");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
         {
