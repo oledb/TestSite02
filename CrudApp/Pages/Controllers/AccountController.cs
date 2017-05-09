629b36b4d60fb76257e42f164da13bc0d74bd3e2
oledb <zf0qwe@gmail.com>
2017-05-09 17:55:59 +0300

    Added Registry and Logout method for application

--- CrudApp/Pages/Controllers/AccountController.cs
+++ CrudApp/Pages/Controllers/AccountController.cs
@@ -9,36 +9,82 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using CrudApp.Models;
+using CrudApp.Models.Account;
 
 namespace CrudApp.Pages.Controllers
 {
     
     public class AccountController : Controller
     {
-        private UserManager<ApplicationUser> _userManager;
-        private SignInManager<ApplicationUser> _signManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signManager)
+            SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
-            _signManager = signManager;
+            _signInManager = signInManager;
+            //_logger = logger;
         }
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Registry(string ReturnUrl = null)
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Registry(RegisterViewModel register, string ReturnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = register.Email, Email = register.Email };
+                var result = await _userManager.CreateAsync(user, register.Password);
+                if (result.Succeeded)
+                {
+                   
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                   // _logger.LogInformation(3, "User created a new account with password.");
+                    return RedirectToLocal("~/Pages/Default/Objective");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(register);
+        }
+
+        [HttpGet]
         public IActionResult Login(string ReturnUrl = null)
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToLocal("~/pages/default/index");
+        }
+
+        [HttpGet]
         public IActionResult ResetPassword(string ReturnUrl = null)
         {
             return View();
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(DefaultController.Index), "Objecitve");
+            }
         }
     }
 }
